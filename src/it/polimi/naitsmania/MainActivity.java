327df8c89e54996a022e8ce4e4407cd642def3d6
Blutimauge <ilpillo90@gmail.com>
2014-02-22 21:01:09 +0100

    here it could be all safe

--- src/it/polimi/naitsmania/MainActivity.java
+++ src/it/polimi/naitsmania/MainActivity.java
@@ -1,8 +1,15 @@
 package it.polimi.naitsmania;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ActionBar;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +17,30 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		TextView firstLink = (TextView) findViewById(R.id.firstLink);
+		TextView secondLink = (TextView) findViewById(R.id.secondLink);
+		// Make the link appears clickable
+
+		firstLink.setMovementMethod(LinkMovementMethod.getInstance());
+		secondLink.setMovementMethod(LinkMovementMethod.getInstance());
+		// Click send to creation of groups
+		firstLink.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), CreateGroupActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		secondLink.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), PreparingToConnection.class);
+				startActivity(intent);
+			}
+		});
+		
 	}
 
 	@Override
