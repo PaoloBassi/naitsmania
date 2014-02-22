4b9dd9ad9db7ead1f01f240640978e8c92b77647
Blutimauge <ilpillo90@gmail.com>
2014-02-22 10:16:52 +0100

    First page ready

---
+++ src/it/polimi/naitsmania/CreateGroupActivity.java
@@ -0,0 +1,16 @@
+package it.polimi.naitsmania;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateGroupActivity extends Activity{
+	
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.connectgroup);
+			
+			
+		}
+	
+}
--- src/it/polimi/naitsmania/MainActivity.java
+++ src/it/polimi/naitsmania/MainActivity.java
@@ -2,7 +2,11 @@ package it.polimi.naitsmania;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		TextView firstLink = (TextView) findViewById(R.id.firstLink);
+		TextView secondLink = (TextView) findViewById(R.id.secondLink);
+		// Make the link appears clickable
+		firstLink.setMovementMethod(LinkMovementMethod.getInstance());
+		secondLink.setMovementMethod(LinkMovementMethod.getInstance());
+		// Click send to creation of groups
+		firstLink.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), CreateGroupActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		secondLink.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), CreateGroupActivity.class);
+				startActivity(intent);
+			}
+		});
+		
 	}
 
 	@Override
